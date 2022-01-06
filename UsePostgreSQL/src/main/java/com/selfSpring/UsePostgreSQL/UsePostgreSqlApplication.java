071ad39d2ed61fd037d97aeb5155b5da6539ddca
Kim-ChanHee <rudnfcks@naver.com>
2022-01-06 15:23:33 +0900

    new project : use postgreSQL

---
+++ UsePostgreSQL/src/main/java/com/selfSpring/UsePostgreSQL/UsePostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.selfSpring.UsePostgreSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsePostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsePostgreSqlApplication.class, args);
+	}
+
+}
